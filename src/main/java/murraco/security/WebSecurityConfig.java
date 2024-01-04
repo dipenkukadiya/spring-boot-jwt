fff1613c9a7289babcbeb9c1482c4b23013c57f6
dipen kukadiya <dipenkukadiya@gmail.com>
2024-01-04 19:18:36 +0530

    forked and jwt

--- src/main/java/murraco/security/WebSecurityConfig.java
+++ src/main/java/murraco/security/WebSecurityConfig.java
@@ -1,7 +1,7 @@
 package murraco.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
